4a8c6e07eb8a26ed606434de069ff9bd6bb14ec1
Frank Queiroz <frank_qsc@hotmail.com>
2017-10-15 11:29:39 -0300

    Teste commit eclise

--- src/Java1/Fatorial.java
+++ src/Java1/Fatorial.java
@@ -8,6 +8,7 @@ public class Fatorial {
 	        for (int i = 1; i < 40; i++) {
 	            fatorial *= i;
 	            System.out.println("Fatorial de "+i+ " = " +fatorial);
+	            System.out.println("lula monstro");
 	        }			
 	}
 }
